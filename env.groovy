e5b8e57f8609112214aa2e6b943b2f257659cd2a
Shiva <tak2siva@gmail.com>
2018-03-14 12:52:10 +0530

    Update scripts

--- env.groovy
+++ env.groovy
@@ -4,11 +4,13 @@ environments {
         username = 'spiuser'
         password = 'spiuser'
         db_host = "localhost"
+        port = 7654
     }
     ci {
         db_name = 'spicinemas'
         username = 'spiuser'
         password = 'spiuser'
         db_host = "db"
+        port = 5432
     }
 }
